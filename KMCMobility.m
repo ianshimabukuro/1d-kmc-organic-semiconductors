7b4cb708eb8c3999611be0a6eed85b396c86d367
ianshimabukuro <140802890+ianshimabukuro@users.noreply.github.com>
2023-07-28 18:52:45 +0900

    Delete KMCMobility.m

--- KMCMobility.m
+++
@@ -1,7 +0,0 @@
-function [Mobility] = KMCMobility(Current,ElectricF)
-%OK
-global Number_Of_Sites Lattice_Constant q Number_Of_Electrons
-
-Mobility= (Current*(Number_Of_Sites-1)*Lattice_Constant)/(q*ElectricF*Number_Of_Electrons);
-
-end
