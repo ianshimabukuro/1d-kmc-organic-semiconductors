c88244b9fcc5747ae89b9bc0d1f2f5174155eba6
ianshimabukuro <140802890+ianshimabukuro@users.noreply.github.com>
2023-07-28 18:51:33 +0900

    Delete EFCont.m

--- EFCont.m
+++
@@ -1,8 +0,0 @@
-function [Energy] = EFCont(SiteEnergy,Electric_Field)
-global Lattice_Constant Number_Of_Sites
-
-for i=1:Number_Of_Sites
-      Energy(i)=SiteEnergy(i)-(i-1)*Lattice_Constant*Electric_Field;
-end
-
-end
